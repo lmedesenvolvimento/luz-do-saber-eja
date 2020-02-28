ae679a649cce26422a773afafe432c07a1f484c6
rodrigoronin <liramailbox@gmail.com>
2020-02-28 10:58:51 -0300

    Ajuste no botão play da homepage mobile

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7e79d75866bb6e1b04f4";
+/******/ 	var hotCurrentHash = "334d06c4ad467bed7464";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
