c88483f5cbffd2fe9056e30713343a8cc6c11dc5
Damis Garcia <damisgarcia@gmail.com>
2020-04-03 17:18:43 -0300

    adicionado novamente pasta docs

---
+++ docs/_next/static/Bdld3K18cq0w2tIZEkOvE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/download":[a],"/index":[a],"/projeto":[a]}}("static\u002Fchunks\u002Ff1b9ff8c2772012082aed8629b7c1c07adf339af.7c76a5b8f5cc94db5cb4.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
