23b72a2e9a77b3979525ba4f355e14d340946374
Damis Garcia <damisgarcia@gmail.com>
2020-04-06 10:20:38 -0300

    atualizado script de deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,7 @@
+git subtree pull --prefix docs origin gh-pages
 npm run export
 touch docs/CNAME
-echo \"eja.luzdosaber.virtual.ufc.br\" >> docs/CNAME
-git add docs/ 
-git commit -m \"Deploy to gh-pages\" 
+echo "eja.luzdosaber.virtual.ufc.br" > docs/CNAME
+git add docs/
+git commit -m "Deploy to gh-pages"
 git subtree push --prefix docs origin gh-pages
